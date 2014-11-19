e6c0c79359ac51f7cc0594860facac04236ab6f8
siquel <squual@kahvipaussi.net>
2014-11-19 18:21:40 +0200

    initial commit

---
+++ SpriterReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content;
+
+namespace SaNi.Spriter
+{
+    public class SpriterReader : ContentTypeReader<SpriterModel>
+    {
+        protected override SpriterModel Read(ContentReader input, SpriterModel existingInstance)
+        {
+            existingInstance = new SpriterModel();
+            return existingInstance;
+        }
+
+    }
+}
