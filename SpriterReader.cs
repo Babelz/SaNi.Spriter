0a44829ac2b8c45872f25d9beef7df3e9c518338
siquel <squual@kahvipaussi.net>
2014-11-20 20:09:06 +0200

    luetaan kansiot ja filut

--- SpriterReader.cs
+++ SpriterReader.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using SaNi.Spriter.Data;
 
@@ -15,8 +16,8 @@ namespace SaNi.Spriter
             string scmlversion = input.ReadString();
             string generator = input.ReadString();
             string generatorVersion = input.ReadString();
-            int folderCount = 0;
-            int entityCount = 0;
+            int folderCount = input.ReadInt32();
+            int entityCount = input.ReadInt32();
             data = new SpriterData(scmlversion, generator, generatorVersion, folderCount, entityCount);
             /*int[] files = new int[folderCount];
 
@@ -25,8 +26,36 @@ namespace SaNi.Spriter
                 // filujen määrä per folder
                 files[i] = input.ReadInt32();
             }*/
+            LoadFolders(input, data, folderCount);
+
 
             return data;
+        }
+
+        private void LoadFolders(ContentReader input, SpriterData data, int folderCount)
+        {
+            for (int folder = 0; folder < folderCount; folder++)
+            {
+                string name = input.ReadString();
+                // folder on id
+                int count = input.ReadInt32();
+                data.AddFolder(new Folder(folder, name, count));
+                LoadFiles(input, data.GetFolder(folder), count);
+            }
+        }
+
+        private void LoadFiles(ContentReader input, Folder folder, int fileCount)
+        {
+            for (int file = 0; file < fileCount; file++)
+            {
+                // file on id
+                string name = input.ReadString();
+                int w = input.ReadInt32();
+                int h = input.ReadInt32();
+                float pivotX = input.ReadSingle();
+                float pivotY = input.ReadSingle();
+                folder.AddFile(new File(file, name, new Vector2(w, h), new Vector2(pivotX, pivotY)));
+            }
         }
 
     }
